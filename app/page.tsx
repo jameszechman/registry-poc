16393bc8a93b2f796bc7df01fd3fac89ca1333d4
James Zechman <jzechman@strongtie.com>
2025-10-19 11:45:06 -0500

    Update registry to add brand

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,18 @@ export default function Home() {
         <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
           <div className="flex items-center justify-between">
             <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A simple hello world component
+              A simple button component
             </h2>
             <OpenInV0Button name="button" className="w-fit" />
           </div>
-          <div className="flex items-center justify-center min-h-[400px] relative">
-            <Button variant="destructive">Hello World</Button>
+          <div className="flex items-center justify-center min-h-[400px] relative flex gap-2">
+            <Button>Button</Button>
+            <Button variant="brand">Button</Button>
+            <Button variant="destructive">Button</Button>
+            <Button variant="outline">Button</Button>
+            <Button variant="ghost">Button</Button>
+            <Button variant="secondary">Button</Button>
+            <Button variant="link">Button</Button>
           </div>
         </div>
       </main>
